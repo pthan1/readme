c080a99bcb1e8f368e24ddd634a2c40f525cd6ca
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-10-30 17:30:59 -0600

    Create nav and searchbar components

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,10 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
+import './SearBar.css'
 
-const SearchBar = () => {
+const SearchBar = ( ) => {
+  const [book, setBook] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    fetch(`googleAPI${book}`)
+    .then(response => response.json())
+    .then()
+    setBook('')
+  }
+  const handleChange = (event) => {
+    setBook(event.target.value)
+  }
+
   return (
     <div className="search-container">
-      <form className="search-form">
-        <input className="search-input" type="text" placeholder="book"></input>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input className="search-input" 
+        type="text" 
+        placeholder="book"
+        value={book}
+        onChange={event => handleChange(event)}
+        />
       </form>
     </div>
   )
