03fa831786681cab2cf1609f2c5346440d47539a
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-10-30 17:28:54 -0400

    Add landing page and basic styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import SearchBar from "../SearchBar/SearchBar"
+import moduleName from "module"
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="landing-body">
+        <p>Login</p>
+        <h1>readME</h1>
+        <p>Welcome to readME, tell us a book you like and we will help you find your next reading</p>
+        <SearchBar />
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const SearchBar = () => {
+  return (
+    <div className="search-container">
+      <form className="search-form">
+        <input className="search-input" type="text" placeholder="book"></input>
+      </form>
+    </div>
+  )
+}
+
+export default SearchBar
