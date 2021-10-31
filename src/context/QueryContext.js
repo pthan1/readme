88499df2dbbf2c41c42d2b0e23db33e333de2e66
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-10-31 14:49:14 -0600

    Handle multiple word queries

--- src/context/QueryContext.js
+++ src/context/QueryContext.js
@@ -1,19 +1,26 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from 'react';
 
-export const QueryContext = createContext()
+export const QueryContext = createContext();
 
 const QueryContextProvider = (props) => {
-  const [bookTitle, setBookTitle] = useState('')
+	const [
+		bookTitle,
+		setBookTitle
+	] = useState('');
 
-  const addBookTitle = (userInput) => {
-    setBookTitle(userInput)
-  }
+	const addBookTitle = (userInput) => {
+		const processedInput = userInput.split(' ');
+		if (processedInput.length === 1) {
+			console.log('in if block', userInput);
+			setBookTitle(userInput);
+		} else {
+			const multipleQuery = processedInput.join('+');
+			console.log('mQ', multipleQuery);
+			setBookTitle(multipleQuery);
+		}
+	};
 
-  return ( 
-    <QueryContext.Provider value={ { bookTitle, addBookTitle } }>
-      {props.children}
-    </QueryContext.Provider>
-   );
-}
- 
+	return <QueryContext.Provider value={{ bookTitle, addBookTitle }}>{props.children}</QueryContext.Provider>;
+};
+
 export default QueryContextProvider;
