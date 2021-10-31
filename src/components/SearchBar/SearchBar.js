b50ad05739268487a7df4d96943cde30295080a5
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-10-31 13:59:22 -0600

    Add router to app and search bar and books container component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -6,19 +6,20 @@ const SearchBar = ( ) => {
   const [book, setBook] = useState('')
   const { addBookTitle } = useContext(QueryContext)
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    addBookTitle(book)
-    setBook('')
-  }
+  // const handleSubmit = (event) => {
+  //   event.preventDefault()
+  //   addBookTitle(book)
+  //   setBook('')
+  // }
   
   const handleChange = (event) => {
     setBook(event.target.value)
+    addBookTitle(event.target.value)
   }
-
+  // onSubmit={event => handleSubmit(event)}
   return (
     <div className="search-container">
-      <form className="search-form" onSubmit={handleSubmit}>
+      <form className="search-form"  action={`search/${book}`} >
         <input className="search-input" 
         type="text" 
         placeholder="book"
