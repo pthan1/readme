8f51dfb83dcb9a90ab835c09f8c08b16280af56f
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-10-31 14:35:45 -0600

    Navigate to search results page and call GoogleBooks API on search

--- src/components/BooksContainer/BooksContainer.js
+++ src/components/BooksContainer/BooksContainer.js
@@ -8,16 +8,21 @@ const BooksContainer = () => {
 		searchResults,
 		setSearchResults
 	] = useState([]);
-	const { bookTitle } = useContext(QueryContext)
+	const { bookTitle } = useContext(QueryContext);
 
-	useEffect((bookTitle) => {
+	useEffect(
+		() => {
+			console.log(bookTitle);
 			fetch(
-			`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&key=AIzaSyBf2vrFs43KCXYdALCcDGm_EeC-3BpS-5w`
-		)
-			.then((response) => response.json())
-			.then((data) => console.log(data));
-	}, [bookTitle]);
-
+				`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&key=AIzaSyBf2vrFs43KCXYdALCcDGm_EeC-3BpS-5w`
+			)
+				.then((response) => response.json())
+				.then((data) => console.log(data));
+		},
+		[
+			bookTitle
+		]
+	);
 
 	const bookCards = searchResults.map((searchResult) => {
 		return (
@@ -35,8 +40,10 @@ const BooksContainer = () => {
 			<div className="card-container">{bookCards}</div>
 		</div>
 	) : (
-		<div><h2>something went wrong</h2></div>
-	)
+		<div>
+			<h2>something went wrong</h2>
+		</div>
+	);
 };
 
 export default BooksContainer;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,34 +1,45 @@
-import React, { useContext, useState } from "react"
-import { QueryContext } from "../../context/QueryContext"
-import './SearchBar.css'
+import React, { useContext, useState } from 'react';
+import { QueryContext } from '../../context/QueryContext';
+import './SearchBar.css';
+import { useHistory } from 'react-router-dom';
 
-const SearchBar = ( ) => {
-  const [book, setBook] = useState('')
-  const { addBookTitle } = useContext(QueryContext)
+const SearchBar = () => {
+	const [
+		book,
+		setBook
+	] = useState('');
+	const { addBookTitle } = useContext(QueryContext);
+	const history = useHistory();
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault()
-  //   addBookTitle(book)
-  //   setBook('')
-  // }
-  
-  const handleChange = (event) => {
-    setBook(event.target.value)
-    addBookTitle(event.target.value)
-  }
-  // onSubmit={event => handleSubmit(event)}
-  return (
-    <div className="search-container">
-      <form className="search-form"  action={`search/${book}`} >
-        <input className="search-input" 
-        type="text" 
-        placeholder="book"
-        value={book}
-        onChange={event => handleChange(event)}
-        />
-      </form>
-    </div>
-  )
-}
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		addBookTitle(book);
+		history.push(`search/${book}`);
+		setBook('');
+	};
 
-export default SearchBar
+	const handleChange = (event) => {
+		setBook(event.target.value);
+		// addBookTitle(event.target.value)
+	};
+	// onSubmit={event => handleSubmit(event)}
+	return (
+		<div className="search-container">
+			<form
+				className="search-form"
+				onSubmit={(event) => handleSubmit(event)}
+				// action={`search/${book}`}
+			>
+				<input
+					className="search-input"
+					type="text"
+					placeholder="book"
+					value={book}
+					onChange={(event) => handleChange(event)}
+				/>
+			</form>
+		</div>
+	);
+};
+
+export default SearchBar;
