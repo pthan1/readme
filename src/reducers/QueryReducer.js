26752e940981fbb9ee4858f99363eb532e7f2dcd
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-11-06 15:05:09 -0600

    Fix indentation

--- src/reducers/QueryReducer.js
+++ src/reducers/QueryReducer.js
@@ -14,21 +14,21 @@ export const queryReducer = (state, action) => {
         bookTitle: multipleQuery,
       }
     }
-      case 'ADD_CATEGORY' :
-        return {
-          ...state,
-          category: action.category,
+    case 'ADD_CATEGORY' :
+      return {
+        ...state,
+        category: action.category,
+      }
+    case 'ADD_BOOK_ID' :
+      return {
+        ...state,
+        bookId: action.bookId,
+      }
+    case 'ADD_OVERVIEW' :
+      return {
+        ...state,
+        overview: action.overview,
         }
-        case 'ADD_BOOK_ID' :
-          return {
-            ...state,
-            bookId: action.bookId,
-          }
-          case 'ADD_OVERVIEW' :
-            return {
-              ...state,
-              overview: action.overview,
-            }
       default : 
         return state
   }
