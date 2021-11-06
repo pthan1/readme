343a4f74c64432c735a450dd8add2a03d0ac64a4
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-11-05 23:19:07 -0600

    Create Reading List view

--- src/components/ReadingList/ReadingList.js
+++ src/components/ReadingList/ReadingList.js
@@ -1,35 +1,41 @@
 import React, { useContext} from "react";
 import { AuthContext } from "../../context/AuthContext";
 import ReadingListCard from "../ReadingListCard/ReadingListCard";
-import RecommendationCard from "../RecommendationCard/RecommendationCard";
 import Nav from "../Nav/Nav";
 import { Link } from "react-router-dom";
-import ReadingList from "../ReadingList/ReadingList"
 
 const ReadingList = () => {
-  const { user } = useContext(AuthContext);
-
+  const { user, isLoggedIn, deleteBookFromReadingList } = useContext(AuthContext);
+// needs deleteBook funcion and button too
+// 
   const readingListCards = user.readingList.map(book => {
   return (
       <Link to="/details">
-        <RecommendationCard
+        <ReadingListCard
           className={"card"}
           imageLinks={book.imageLinks}
           title={book.title}
           key={book.key}
           id={book.id}
           overview={book.overview}
+          deleteBookFromReadingList={deleteBookFromReadingList}
         />
       </Link>
     )})
 
 return (
-    <div className="recommendation-view">
+    <div className="reading-list-view">
         <Nav />
-        <div className="display-body-recommendation">
-          <p className="p-prompt-recommendation">Your Reading List</p>
-          <div className="card-container-recommendation">{recommendationCards}</div>
+        <div className="display-body-reading-list">
+          <p className="p-prompt-reading-list">Your Reading List</p>
+          <div className="card-container-reading-list">
+          {/* if a user is logged in, show rading list cards, if rading list cards is empty show error
+          if a user is not logged in, shw cards that have been added, if no cards, then don't show anything */}
+          
+          {readingListCards}</div>
         </div>
       </div>
   )
 }
+
+export default ReadingList
