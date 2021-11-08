5846e6150088334b779cee9fd55ef7c85ae9df30
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-11-07 20:15:50 -0500

    Add resposive design to reading list

--- src/components/ReadingListCard/ReadingListCard.js
+++ src/components/ReadingListCard/ReadingListCard.js
@@ -16,7 +16,7 @@ const ReadingListCard = ({ imageLinks, title, id, overview }) => {
       <img src={imageLinks} alt={`${title} cover`} />
       <div className="rl-card-detail">
         <p className="rl-card-title">{title}</p>
-        <p>
+        <p className="top-cat">
           Top Cat! The most effectual Top Cat! Who’s intellectual close friends get to call him T.C., providing it’s
           with dignity. Top Cat! The indisputable leader of the gang. He’s the boss, he’s a pip, he’s the championship.
           He’s the most tip top, Top Cat.
